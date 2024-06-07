47e49e3c700a097e0dda243d1911f3e40e167821
Abbiraam Sinnathamby <169357@student.horizoncollege.nl>
2024-06-07 23:23:09 +0200

    update counter

--- Website/wordpress/wp-content/themes/kenta-creato/template-parts/patterns/counters.php
+++ Website/wordpress/wp-content/themes/kenta-creato/template-parts/patterns/counters.php
@@ -2,7 +2,7 @@
 <div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"radius":"24px"}},"backgroundColor":"kenta-base-100","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-kenta-base-100-background-color has-background" style="border-radius:24px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"typography":{"fontStyle":"normal","fontWeight":"800"},"elements":{"link":{"color":{"text":"var:preset|color|kenta-accent"}}}},"textColor":"kenta-accent","fontSize":"kenta-font-xlarge"} -->
-<p class="has-text-align-center has-kenta-accent-color has-text-color has-link-color has-kenta-font-xlarge-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-style:normal;font-weight:800"><?php esc_html_e('10+', 'kenta-creato'); ?></p>
+<p class="has-text-align-center has-kenta-accent-color has-text-color has-link-color has-kenta-font-xlarge-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-style:normal;font-weight:800"><?php esc_html_e('100+', 'kenta-creato'); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"typography":{"textTransform":"uppercase"}},"fontSize":"kenta-font-small"} -->
